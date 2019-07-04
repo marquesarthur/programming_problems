a6f49a402f229f956bb67ec76d4c936f9e17364b
Arthur Marques <marques.s.arthur@gmail.com>
2019-07-03 17:11:11 -0700

    brute force find name given precedence

--- interviewbit/interviewbit/tall/find_word.py
+++ interviewbit/interviewbit/tall/find_word.py
@@ -1,51 +1,44 @@
-#
-#
-#
-#
-#
-#
-#
-#
-# def insert_character(word, new_letter, before):
-#
-#     if new_letter in word:
-#         return word
-#
-#     result = ""
-#     for c in word:
-#         if c == before:
-#             result = new_letter + c
-#         else:
-#             result = result + c
-#
-#     if new_letter not in result:
-#         result += new_letter
-#
-#     return result
-#
-#
-#
-#
-#
-# def findWord(rules):
-#     rules = sorted(rules, cmp=sort_by_rules)
-#     word = ""
-#     for r in rules:
-#         left, right = r.split(">")[0], r.split(">")[1]
-#
-#         if not word:
-#             word = left+right
-#         else:
-#             word = insert_character(word, left, right)
-#             word = insert_character(word, right, right)
-#     return word
-#
-#
-#
-# # print(findWord(["P>E","E>R","R>U"])) # // PERU
-# # print(findWord(["I>N","A>I","P>A","S>P"])) # // SPAIN
-# #
-# # print(findWord(["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"])) # // HUNGARY
-# # print(findWord(["I>F", "W>I", "S>W", "F>T"])) # // SWIFT
-# # print(findWord(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"])) # // SWITZERLAND
-# print(findWord(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"]))  #// PORTUGAL
+
+
+
+
+# Brute force approach!
+def findWord(rules):
+    precedence_map = {}
+    inverse_precedence_map = {}
+    letters = set()
+    for r in rules:
+        left, right = r.split(">")[0], r.split(">")[1]
+        precedence_map[left] = right
+        if right not in precedence_map:
+            precedence_map[right] = precedence_map[left]
+        letters.add(left)
+        letters.add(right)
+
+    stack = list(letters)
+
+    iterations = 0
+
+    while iterations <= 1000:
+
+        for letter, before in precedence_map.items():
+
+            if letter != before:
+                stack.pop(stack.index(letter))
+                stack.insert(stack.index(before), letter)
+
+        iterations += 1
+
+        # if letter in inverse_precedence_map:
+        #     recursive_insert_left(stack, letter, inverse_precedence_map[letter])
+
+    return "".join(stack)
+
+
+print(findWord(["P>E","E>R","R>U"])) # // PERU
+print(findWord(["I>N","A>I","P>A","S>P"])) # // SPAIN
+
+print(findWord(["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"])) # // HUNGARY
+print(findWord(["I>F", "W>I", "S>W", "F>T"])) # // SWIFT
+print(findWord(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"])) # // SWITZERLAND
+print(findWord(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"]))  # // PORTUGAL
