7347dbdb6ca4aa6ef574ee6c97ced8c3d8610bd8
Arthur Marques <marques.s.arthur@gmail.com>
2019-07-11 11:23:00 -0700

    find word map

--- interviewbit/interviewbit/tall/find_word.py
+++ interviewbit/interviewbit/tall/find_word.py
@@ -36,39 +36,6 @@ def findWord(rules):
 
 
 
-
-
-def findWord(rules):
-    precedence_map = {}
-    letters = set()
-    for r in rules:
-        left, right = r.split(">")[0], r.split(">")[1]
-        precedence_map[left] = right
-        if right not in precedence_map:
-            precedence_map[right] = precedence_map[left]
-        letters.add(left)
-        letters.add(right)
-
-    stack = list(letters)
-
-    iterations = 0
-
-    while iterations <= 1000:
-
-        for letter, before in precedence_map.items():
-
-            if letter != before:
-                stack.pop(stack.index(letter))
-                stack.insert(stack.index(before), letter)
-
-        iterations += 1
-
-        # if letter in inverse_precedence_map:
-        #     recursive_insert_left(stack, letter, inverse_precedence_map[letter])
-
-    return "".join(stack)
-
-
 print(findWord(["P>E","E>R","R>U"])) # // PERU
 print(findWord(["I>N","A>I","P>A","S>P"])) # // SPAIN
 print(findWord(["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"])) # // HUNGARY
