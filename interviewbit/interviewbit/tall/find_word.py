0e0c3f3f89e181d7d7ba769d2101e614c23aa671
Arthur Marques <marques.s.arthur@gmail.com>
2019-07-11 11:25:07 -0700

    new find word

--- interviewbit/interviewbit/tall/find_word.py
+++ interviewbit/interviewbit/tall/find_word.py
@@ -41,4 +41,4 @@ print(findWord(["I>N","A>I","P>A","S>P"])) # // SPAIN
 print(findWord(["U>N", "G>A", "R>Y", "H>U", "N>G", "A>R"])) # // HUNGARY
 print(findWord(["I>F", "W>I", "S>W", "F>T"])) # // SWIFT
 print(findWord(["W>I", "R>L", "T>Z", "Z>E", "S>W", "E>R", "L>A", "A>N", "N>D", "I>T"])) # // SWITZERLAND
-print(findWord(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"]))  # // PORTUGAL
+# print(findWord(["R>T", "A>L", "P>O", "O>R", "G>A", "T>U", "U>G"]))  # // PORTUGAL
